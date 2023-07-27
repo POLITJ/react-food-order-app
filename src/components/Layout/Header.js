3d378826da3a56a7347d44b7fb7372aea1c681d8
Jose Polit <jospolit@gmail.com>
2023-07-27 20:03:02 +0200

    add cart componenet with dummy data and closing backdrop functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>React Bistrot</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of food" />
